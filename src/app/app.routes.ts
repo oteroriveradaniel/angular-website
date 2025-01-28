7befd15449385a8c2ddda3ad02e9ae34d18b5a3a
dor <dor.webdeveloper@gmail.com>
2025-01-28 17:11:16 +0100

    Landing page + routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { DevPageComponent } from './dev-page/dev-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: LandingPageComponent }, 
+  { path: "portfolio", component: PortfolioPageComponent }, 
+  { path: "dev", component: DevPageComponent }, 
+];
---
+++ src/app/dev-page/dev-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dev-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './dev-page.component.html',
+  styleUrl: './dev-page.component.scss'
+})
+export class DevPageComponent {
+
+}
